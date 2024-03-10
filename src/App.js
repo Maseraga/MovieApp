f2829141e454d5c61dac5750cab16e0e8652df42
Paulina <maseragapaulina609@gmail.com>
2024-03-10 14:09:23 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'https://www.omdbapi.com?apikey=7e6893bf';
+const API_URL = 'http://www.omdbapi.com?apikey=7e6893bf';
 const movie = {
     Title: "Barbie Fairytopia: Mermaidia",
     Type: "movie",
